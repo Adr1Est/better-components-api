207b5e11d2a65eab23d7bc7095eef7b8f2240e09
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-06 12:48:12 +0200

    getConversationMessages controller

--- src/controllers/v1/conversation.controller.ts
+++ src/controllers/v1/conversation.controller.ts
@@ -30,6 +30,22 @@ export class ConversationController {
     }
   }
 
+  static getConversationMessages = async (req: Request, res: Response) => {
+    const id = req.params.id as string; 
+
+    try {
+      const conversation = await ConversationModel.getConversationWithMessages(id);
+      
+      if (!conversation) {
+        return res.status(404).json({ msg: "La conversación no existe" });
+      }
+
+      return res.status(200).json({ conversation });
+    } catch (error) {
+      return res.status(500).json({ msg: "Error interno del servidor" });
+    }
+  }
+
   static createConversation = async (req: Request, res: Response) => {
     const id = req.params.id as string;
     const { title } = req.body;
