2915dd9b2b5c94f3c40265818c006a6ce67d2e5a
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-06 12:50:10 +0200

    ruta para mensajes

--- src/routes/v1/conversation.routes.ts
+++ src/routes/v1/conversation.routes.ts
@@ -5,6 +5,7 @@ export const conversationRoutes = Router();
 
 conversationRoutes.get("/", ConversationController.getAllConversations);
 conversationRoutes.get("/:id", ConversationController.getConversationsByUser);
+conversationRoutes.get("/:id/messages", ConversationController.getConversationMessages);
 conversationRoutes.post("/:id", ConversationController.createConversation);
 conversationRoutes.put("/:id", ConversationController.changeTitle);
 conversationRoutes.delete("/:id", ConversationController.deleteConversationById);
