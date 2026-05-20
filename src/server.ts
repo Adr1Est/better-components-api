8cc4c2330ca2ce0226c347121e70b76535dec181
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-05-20 12:13:16 +0200

    socket io

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,37 @@
 import http from 'http';
+import { Server } from 'socket.io';
 import app from '@/app';
-import { envs } from './config/envs';
+import { envs } from '@/config/envs';
 
 const PORT: number = envs.port;
 
 const server = http.createServer(app);
+
+// Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: envs.origin,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Cliente conectado: ${socket.id}`);
+
+  socket.emit("connected", {
+    message: "WebSocket conectado",
+  });
+
+  socket.on("message", (data) => {
+    console.log("Mensaje recibido:", data);
+
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Cliente desconectado: ${socket.id}`);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Servidor funcionando en puerto: ${PORT}`);
